c0bbd0c23571316195d6013bb2368a9ca991ec34
dmblock <zh15904837658@163.com>
2024-02-15 19:21:35 +0800

    修复了十分傻逼的错误

--- index.sh
+++ index.sh
@@ -24,7 +24,7 @@ update_time() {
 
 # 检索版本列表
 version_input_check="$(cat versions.txt)"
-version_list="$(curl -s https://git.lantian.pro/ltcontainer/peeditor_down/raw/master/version_list/versions.txt)"
+version_list="$(curl -s https://git.lantian.pro/ltcontainer/preloader_down/raw/master/version_list/versions.txt)"
 update_time
 echo -e "${current_time} ${log_info} 正在开始下载PreLoader"
 update_time
